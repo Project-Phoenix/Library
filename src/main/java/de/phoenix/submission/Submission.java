b2a14d8ff6932e33662f192507337c0f7c246758
Meldanor <Bhaals@gmx.de>
2013-04-14 20:10:57 +0200

    Add submission interface

---
+++ src/main/java/de/phoenix/submission/Submission.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2013 Project-Phoenix
+ * 
+ * This file is part of library.
+ * 
+ * library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ * 
+ * library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.phoenix.submission;
+
+public interface Submission {
+
+    public String getAuthor();
+
+    public String getText();
+}
